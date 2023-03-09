aa7ea74ae375473e0e1c2395c59a66228cf03154
alexander.petrovskiy <tula147@yandex.ru>
2023-03-09 15:13:39 +0400

    # vsCode tutorial

---
+++ vsCodeTips.py
@@ -0,0 +1,116 @@
+# SHORTCUTS
+
+# COMMON OPTIONS
+ctrl + , # open settings
+ctrl + k, ctrl + s # open shortcuts
+left down gear # many useful commands
+ctrl + ` # open terminal
+code + folder name # to open folder in VSCode
+code . # type in a terminal to open current directory in VSCode
+
+# SIDEBAR
+ctrl + b # toggle left sidebar
+ctrl + shift + e # open explorer tab
+ctrl + shift + f # open find tab
+ctrl + shift + g # open git tab
+ctrl + shift + d # open debub tab
+ctrl + shift + x # open extentions tab
+
+# COMMAND PALETTE
+ctrl + shift + p or F1 # command palette
+Zen mode # show just code
+Toggle minimap # show minimap of your code as scrollbar
+Publish to GitHub # to create repository on your GitHub
+Theme # to change theme
+
+# FILES HANDLING
+ctrl + tab # switch between file tabs
+ctrl + p, type file name to open
+ctrl + w # close current tab
+ctrl + shift + T # reopen previously closed tab
+ctrl + K, M # change file language
+ctrl + k, r # show a current file in provider
+
+# SEARCH AND NAVIGATION
+F8 # go to next problem or warning
+shift + F8 # go to previous problem or warning
+alt + <- / -> # go forward / backward
+ctrl + shift + o # find difinition of function or variable in file
+shift + F12 # show references
+alt + F12 # show definition 
+F12 # go to definition
+ctrl + R # change project
+ctrl + shift + space # hint for parameters in function
+
+# LINES / WORDS HANDLING
+alt + arrow up / down # move line up / down
+alt + shift + arrow up / down # copy line up / down
+ctrl + k, ctrl + ] / [ # unfold / fold region
+ctrl + k, ctrl + o / j # fold / unfold all regions
+ctrl + shift + \ # jump to matched bracket
+ctrl + G # go to line
+ctrl + L # select current line
+
+ctrl + d # select word you are on
+ctrl + d, ctrl + f # select and find
+F2 # rename variable
+
+
+
+# EXTENTIONS
+# ADVANCED FILE CREATE EXTENTION
+# 1) open palette -> Advanced new file or ctrl + alt + n 
+# 2) select directory
+# 3) enter file name
+
+# QUOKKA
+# Quokka extention - playground for javascript file
+# open command palette => quokka
+# ctrl + k, e to stop on file or command palette quokka stop current
+
+# SETTINGS SYNC extension - sync your settings
+
+# TURBO CONSOLE LOG extension
+# ctrl + alt + L - create console.log
+# alt + shift + c - comment all console.log
+
+
+
+# SNIPPETS
+! + tab # html template
+link + tab # create link tag
+tag_name + tab # create tag
+.className + tab # create div with this class
+.className * 3 # create three divs with this class
+
+
+nav>ul>li # hierarcical tags
+<nav>
+  <ul>
+    <li></li>
+  </ul>
+</nav>
+
+a{Click Me} # tag with content
+<a href="">Click Me</a>
+
+div.first-class.second-class # multiple classes
+<div class="first-class second-class"></div>
+
+div#content.test # id and class
+<div id="content" class="test"></div>
+
+# List
+li>3{Line number $}
+<li>Line number 1</li>
+<li>Line number 2</li>
+<li>Line number 3</li>
+
+# Inline list
+ul.list-inline>li.list-inline-item{Line number $}*3
+<ul class="list-inline">
+	<li class="list-inline-item">Line number 1</li>
+	<li class="list-inline-item">Line number 2</li>
+	<li class="list-inline-item">Line number 3</li>
+</ul>
+        
